2090434df2028a708ea7e3d913e99a58769eed25
Hassan Ragab <ragab.hassan@outlook.com>
2024-05-27 18:00:07 +0200

    add warning component

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
+import Warning from "./Warning";
 
 export default function Textarea() {
+  const [text, setText] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
+  const [warningText, setWarningText] = useState("");
+
+  const handleChange = (event) => {
+    let newText = event.target.value;
+    if (newText.includes("<script>")) {
+      newText = newText.replace("<script>", "");
+      setShowWarning(true);
+      setWarningText("No <script> tag allowed.");
+    } else if (newText.includes("@")) {
+      newText = newText.replace("@", "");
+      setShowWarning(true);
+      setWarningText("No @ symbol allowed.");
+    }
+
+    setText(newText);
+  };
   return (
-    <textarea className="textarea"
-    placeholder="Enter your text"
-    spellCheck="false"
-    onChange={(e)=>{
-        console.log(e.target.value);
-    }}/>
-  )
+    <div className="textarea">
+      <textarea
+        value={text}
+        placeholder="Enter your text"
+        spellCheck="false"
+        onChange={handleChange}
+      />
+      {showWarning && <Warning warningText={warningText} />}
+    </div>
+  );
 }
